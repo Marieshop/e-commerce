f645ad934124686b38974d677229ada65ad2888b
Marieshop <nshimemarie@gmail.com>
2026-04-13 12:50:14 +0200

    Update HomePage.tsx

--- HomePage.tsx
+++ HomePage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Product } from '../types';
 import { api } from "./api";
-import { ProductCard } from '../ProductCard';
+import  ProductCard  from './ProductCard';
 import { CategoryFilter } from '../CategoryFilter';
 import { Input } from '../Input';
 import { Button } from '../Button';
